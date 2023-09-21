16a2c9ff981d2dad68caf28791d6418fa519424a
jarltuxen <jarl.tuxen@gmail.com>
2023-09-21 15:49:44 +0200

    kommentarer

--- src/main/java/com/example/jpastudentprep/dto/StudentDTO.java
+++ src/main/java/com/example/jpastudentprep/dto/StudentDTO.java
@@ -5,8 +5,10 @@ import com.example.jpastudentprep.model.Student;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+//immutable DTO
 public record StudentDTO(int id, String name, LocalDate bornDate, LocalTime bornTime) {
+    /* alternativ placering af toEntity, hvis det kun er simple konverteringer
     public Student toEntity() {
         return new Student(id(), name(), bornDate(), bornTime());
-    }
+    }*/
 }
